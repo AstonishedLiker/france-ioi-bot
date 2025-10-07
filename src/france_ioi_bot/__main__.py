631f3fa3ba238d871aff80c4f6ec426cb47d3095
AstonishedLiker <astonishedliker@users.noreply.github.com>
2025-10-07 22:34:00 +0200

    use argparse

--- src/france_ioi_bot/__main__.py
+++ src/france_ioi_bot/__main__.py
@@ -1,14 +1,25 @@
+import os
+from urllib.parse import quote
+import requests
+import argparse
 from france_ioi.Account import *
 
 def main():
-    phpSessId: str = input("Please enter your PHPSESSID token: ") # TODO: Use argparse to get this from command line arguments instead of input()
-    account = Account(phpSessId)
+    parser = argparse.ArgumentParser(
+        prog='france-ioi-bot',
+        description='A bot that tries to complete all challenges on the France-IOI platform.',
+        epilog='')
+
+    parser.add_argument("-t", "--token", help="The required PHPSESSID token", required=True)
+    args = parser.parse_args()
+
+    account = Account(args.token)
     if not account.hasSuccessfullyInitialized:
         print("Authentication failed!")
         exit(1)
 
     print(f"Logged in user @{account.username}!")
-    print(f"Enumerating all chapters and their tasks...")
+    print("Enumerating all chapters and their tasks...")
 
     levels = account.queryLevels()
     if levels is None:
@@ -20,7 +31,7 @@ def main():
         for chapter in level.chapters:
             print(f"\t- {chapter.title} ({chapter.link})")
             for task in chapter.tasks:
-                print(f"\t\t- [{'X' if task.isFinished else ' '}] {task.title} ({task.link})")
+                print(f"\t\t- [{'X' if task.isFinished else ' '} - {'SVE' if os.path.exists(f'{level.title}/{chapter.title}/{task.title}') else 'NSV'}] {task.title} ({task.category})")
 
 if __name__ == "__main__":
     main()
