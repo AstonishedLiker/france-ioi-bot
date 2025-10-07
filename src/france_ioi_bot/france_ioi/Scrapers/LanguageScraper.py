6e1126d1fc2eab3d0b73a9aeb07441d2cebadf4b
AstonishedLiker <astonishedliker@users.noreply.github.com>
2025-10-07 22:37:03 +0200

    add language check

---
+++ src/france_ioi_bot/france_ioi/Scrapers/LanguageScraper.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from bs4 import BeautifulSoup
+from france_ioi.Language import Language
+
+def scrape_language(doc: BeautifulSoup) -> Optional[Language]:
+    languageDiv = doc.find("div", { "class": "menu-language-select" })
+    if languageDiv is None:
+        print(":: Error scrapping the France-IOI home page: cannot find the language div")
+        return None
+
+    languageListDiv = languageDiv.div
+    assert languageListDiv is not None
+
+    languageImg = languageListDiv.img
+    assert languageImg is not None
+
+    languageTitle = languageImg["title"]
+    return Language(languageTitle)
