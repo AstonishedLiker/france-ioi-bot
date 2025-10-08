f6e771a32b1ec61875daaa8e01ae756c3d5f4a7f
AstonishedLiker <AstonishedLiker@users.noreply.github.com>
2025-10-08 23:33:13 +0200

    add support for some previously unsupported chapters

--- src/france_ioi_bot/france_ioi/Scrapers/TaskScraper.py
+++ src/france_ioi_bot/france_ioi/Scrapers/TaskScraper.py
@@ -9,25 +9,19 @@ from france_ioi.Constants import FRANCEIOI_BASE_URL
 if TYPE_CHECKING:
     from france_ioi.Account import Account
 
-async def scrape_tasks(account: Account, chapter: Chapter) -> bool:
+def scrape_tasks(account: Account, chapter: Chapter) -> bool:
     response = account.getHttpQueryAuthed(chapter.link.removeprefix(FRANCEIOI_BASE_URL))
     if response is None:
         return False
+
     doc = BeautifulSoup(response.content.decode(), "html.parser")
-    exercises = doc.find("div", { "class": "chapter-tasks with-categories" })
-    if exercises is None:
-        print(f"!! Cannot find the exercises body div, the chapter may not be supported ({chapter.link})")
+    taskListDiv = doc.find("div", { "class": "chapter-tasks" })
+    if taskListDiv is None:
+        print(f"!! Cannot find tasks for chapter '{chapter.title}' ({chapter.link})")
         return False
 
-    for exerciseDiv in exercises.find_all("div", { "class": "chapter-item-row" }):
-        categorySpan = exerciseDiv.find("span", { "class": "chapter-item-category" })
-        assert categorySpan is not None
-
-        categoryTextSpan = categorySpan.span
-        assert categoryTextSpan is not None
-
-        category = categoryTextSpan["class"]
-        titleSpan = exerciseDiv.find("span", { "class": "chapter-item-title" })
+    for taskDiv in taskListDiv.find_all("div", { "class": "chapter-item-row" }):
+        titleSpan = taskDiv.find("span", { "class": "chapter-item-title" })
         assert titleSpan is not None
 
         anchor = titleSpan.a
@@ -37,15 +31,34 @@ async def scrape_tasks(account: Account, chapter: Chapter) -> bool:
         link = anchor["href"]
         assert isinstance(link, str)
 
-        stateImg = exerciseDiv.img
+        categorySpan = taskDiv.find("span", { "class": "chapter-item-category" })
+        if categorySpan:
+            categoryTextSpan = categorySpan.span
+            assert categoryTextSpan is not None
+            assert categoryTextSpan["class"] is not None
+            categoryStr: str = categoryTextSpan["class"][0]
+            category = TaskCategory(categoryStr)
+        else:
+            chapterItemType = doc.find("img", { "class": "chapter-item-type" })
+            if chapterItemType is None:
+                print(f"!! Cannot find task type for chapter '{chapter.title}' ({chapter.link})")
+                continue
+            titleType = chapterItemType["title"]
+            if titleType == "Cours":
+                category = TaskCategory("CategoryCourse")
+            elif titleType == "Sujet":
+                category = TaskCategory("CategoryValidation")
+            else:
+                print(f"!! Unknown chapter item type '{chapterItemType['title']}' for chapter '{chapter.title}' ({chapter.link})")
+                continue
+
+        stateImg = taskDiv.find("img", { "class": "chapter-item-state" })
         assert stateImg is not None
 
         stateTitle = stateImg["title"]
         assert isinstance(stateTitle, str)
 
-        category = TaskCategory(category[0])
         isFinished = stateTitle.startswith("Lu") if category == TaskCategory.COURSE else stateTitle.startswith("Terminé")
-
         chapter.tasks.append(Task(taskTitle, link, isFinished, category, chapter))
 
     return True
