9d33a82f2d662441ad98490c7fede525b025512e
AstonishedLiker <AstonishedLiker@users.noreply.github.com>
2025-10-08 23:33:59 +0200

    add `hasSuccessfullyInitialized` assertion to GET and POST methods

--- src/france_ioi_bot/france_ioi/Account.py
+++ src/france_ioi_bot/france_ioi/Account.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from typing import Optional
-from requests import Session, Response
+import requests
 
 from france_ioi.Scrapers.ChapterLevelScraper import scrape_levels_chapters
 from france_ioi.Scrapers.UsernameScraper import scrape_username
@@ -10,44 +10,49 @@ from france_ioi.Constants import FRANCEIOI_BASE_URL
 
 class Account():
     def __init__(self, phpSessId: str):
-        self.session = Session()
+        self.phpSessId = phpSessId
+        self.session: requests.Session = requests.Session()
+        self.session.cookies.set("PHPSESSID", phpSessId)
         self.hasSuccessfullyInitialized = False
         self.username: Optional[str] = None
-        self.session.cookies["PHPSESSID"] = phpSessId
         self.initialize()
 
-    def getHttpQueryAuthed(self, subdomain: str) -> Optional[Response]:
-        response = self.session.get(FRANCEIOI_BASE_URL + subdomain)
-        if not response.ok:
-            print(f":: Error GET France-IOI ({subdomain}), expecting success, got error code {response.status_code})!")
-            return None
-        return response
-
-    def postHttpQueryAuthed(self, subdomain: str, data: dict) -> Optional[Response]:
-        response = self.session.post(FRANCEIOI_BASE_URL + subdomain, data=data)
-        if not response.ok:
-            print(f":: Error POST France-IOI ({subdomain}), expecting success, got error code {response.status_code})!")
-            return None
-        return response
-
-    # TODO: Check for language (as it will mess up the scraper)
     def initialize(self):
-        assert self.hasSuccessfullyInitialized == False
+        assert not self.hasSuccessfullyInitialized
 
-        response = self.getHttpQueryAuthed(f"/algo/chapters.php")
+        response = self.getHttpQueryAuthed(f"/algo/chapters.php", True)
         if response is None:
             return
 
         doc = BeautifulSoup(response.content.decode(), "html.parser")
         language = scrape_language(doc)
         if language is not None and language != Language.FRENCH:
-            print(":: Please set your language to french in the France-IOI user interface!")
+            print(":: Please set your language to french in the France-IOI user interface")
             return
         elif language is None:
             return
 
         self.username = scrape_username(doc)
         self.hasSuccessfullyInitialized = isinstance(self.username, str)
+
+    def getHttpQueryAuthed(self, subdomain: str, bypassInitCheck: bool = False) -> Optional[requests.Response]:
+        if not bypassInitCheck:
+            assert self.hasSuccessfullyInitialized
+
+        response = self.session.get(FRANCEIOI_BASE_URL + subdomain)
+        if not response.ok:
+            print(f":: Error GET France-IOI ({subdomain}), expecting success, got error code {response.status_code})")
+            return None
+        return response
+
+    def postHttpQueryAuthed(self, subdomain: str, data: dict) -> Optional[requests.Response]:
+        assert self.hasSuccessfullyInitialized
+
+        response = self.session.post(FRANCEIOI_BASE_URL + subdomain, data=data)
+        if response.status_code not in [200, 201, 204]:
+            print(f":: Error POST France-IOI ({subdomain}), expecting success, got error code {response.status_code})")
+            return None
+        return response
 
     def queryLevels(self):
         assert self.hasSuccessfullyInitialized
